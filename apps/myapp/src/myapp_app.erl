a191c0abdee5b17889d7a500f1f30e31597064d8
Дамјан Георгиевски <gdamjan@gmail.com>
2026-08-08 20:59:52 +0200

    initial commit

---
+++ apps/myapp/src/myapp_app.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%% @doc myapp public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(myapp_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    {ok, [SettingsFile]} = init:get_argument(conf),
+    logger:debug("Settings file: ~p", SettingsFile),
+    myapp_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
