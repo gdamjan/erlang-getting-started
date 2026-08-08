9c400fdaf1f918fe8a28582aa95edfc5b7acc363
Дамјан Георгиевски <gdamjan@gmail.com>
2026-08-08 23:30:52 +0200

    document/comment where this is coming from

--- apps/myapp/src/myapp_log_formatter.erl
+++ apps/myapp/src/myapp_log_formatter.erl
@@ -1,5 +1,7 @@
 -module(myapp_log_formatter).
 
+%%% Global log formatter handler.
+%%% Check `config/sys.config.src` to see how its enabled.
 -export([format/2]).
 
 format(#{level := Level, msg := {report, Msg}}, _Config) ->
@@ -10,6 +12,8 @@ format(#{level := Level, msg := {Fmt, Args}}, _Config) ->
     Prefix = printk_prefix(Level),
     [Prefix, io_lib:format(Fmt, Args), "\n"].
 
+%%% Loglevel formatting according to the systemd/printk convention
+%%% https://www.freedesktop.org/software/systemd/man/latest/sd-daemon.html#Description
 printk_prefix(emergency) -> "<0>";
 printk_prefix(alert)     -> "<1>";
 printk_prefix(critical)  -> "<2>";
